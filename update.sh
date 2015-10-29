98baf5dbd7fdd8d8dd1e52fdece77056972f05fc
Calle Kabo <calle.kabo@gmail.com>
2015-10-30 00:08:48 +0100

    added notice

--- update.sh
+++ update.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
+NOTICE='<div style=\"padding: 1em; background-color: #d9edf7; border-color: #bce8f1; color: #3a87ad; border-radius: 4px; margin-bottom: 1em;\">Frihetsportalen.se har tekniska problem för tillfället. Du tittar på den senaste backupen som finns tillgänglig, vilket innebär att du kan endast läsa artiklarna på framsidan. Inga länkar, kommentarsfunktioner, etc. fungerar för tillfället.</div>'
+
 cd mirror
 httrack --update
 # check that files were updated
 cd ..
 rm -r index.html wp-*
-mv mirror/www.frihetsportalen.se/* ./
+cp -r mirror/www.frihetsportalen.se/* ./
+sed "/<div id=\"content\"/a ${NOTICE}" index.html -i
 # check if we need git commit
-git add index.html wp-*
-git commit -m "automatic update"
-git push
+#git add index.html wp-*
+#git commit -m "automatic update"
+#git push
